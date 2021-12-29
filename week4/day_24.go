9c669197ae68989b0c8bf8cf7bfc7835e14eae1a
Jesse Somerville <jssomerville2@gmail.com>
2021-12-28 20:43:14 -0500

    day 24 solution

--- week4/day_24.go
+++ week4/day_24.go
@@ -2,80 +2,48 @@ package week4
 
 import (
 	"fmt"
-	"math"
 )
 
+// MONAD finds the largest valid MONAD accepted by the ALU.
 func MONAD(f string) {
-
-	// targets := map[int]bool{0: true}
-	// maxTarget := 1
-	// for opIdx := len(ops) - 1; opIdx >= 0; opIdx-- {
-	// 	targets, maxTarget = findBounds(ops[opIdx], targets, maxTarget)
-	// }
-
-	wz := WZ{123, 456}
-	nwz := wz.Update(9, 678)
-	fmt.Println(nwz.W, nwz.Z)
-
-	// input := 111111111
-	// var z, val int
-	// for i := 0; i < 9; i++ {
-	// 	val, input = splitMSD(input)
-	// 	z = ops[i].Op(val, z)
-	// }
-	// fmt.Println(z)
-
-}
-
-type MONADRev struct {
-	Ops         []InstBlock
-	ValidMONADs []*WZ
-}
-
-func (mr *MONADRev) Reverse(opIdx, targetOut int) []*WZ {
-	// op := mr.Ops[opIdx]
-	// for z := 0; z <= (targetOut+1)*26; z++ {
-	// 	for w, res := range op.AllWs(z) {
-	// 		if res == targetOut {
-
-	// 		}
-	// 	}
-	// }
-	return nil
-}
-
-type WZ struct {
-	W, Z int
-}
-
-func (wz WZ) Update(w, z int) *WZ {
-	nextPow := int(math.Log10(float64(wz.W))) + 1
-	newW := (wz.W) + (w * int(math.Pow10(nextPow)))
-	return &WZ{
-		W: newW,
-		Z: z,
+	validMONADS := ReverseMONAD(len(ops)-1, 0)
+	maxMONAD := 0
+	minMONAD := int(^uint(0) >> 1)
+	for _, m := range validMONADS {
+		if m.MONAD > maxMONAD {
+			maxMONAD = m.MONAD
+		}
+		if m.MONAD < minMONAD {
+			minMONAD = m.MONAD
+		}
 	}
+	fmt.Println("MAX:", maxMONAD, "MIN:", minMONAD)
 }
 
-func findBounds(op InstBlock, targets map[int]bool, maxTarget int) (map[int]bool, int) {
-	switch inst := op.(type) {
-	case *AddInst:
-		op = inst
-	case *SubInst:
-		op = inst
+// ReverseMONAD recursively finds valid MONADs by running the program in reverse.
+func ReverseMONAD(opIdx, targetOut int) []*ProgramInput {
+	if opIdx < 0 {
+		return nil
 	}
+	out := []*ProgramInput{}
 
-	allowedInputs := make(map[int]bool)
-	maxInput := -1
-	for z := 0; z <= (maxTarget+1)*26; z++ {
-		for _, res := range op.AllWs(z) {
-			if targets[res] {
-				allowedInputs[z] = true
-				if z > maxInput {
-					maxInput = z
-				}
+	op := ops[opIdx]
+	inputs := op.GenInput(targetOut)
+	for w, inp := range inputs {
+		if opIdx == 0 {
+			out = append(out, &ProgramInput{w, inp})
+		} else {
+			for _, prev := range ReverseMONAD(opIdx-1, inp) {
+				prev.MONAD = (prev.MONAD * 10) + w
+				out = append(out, prev)
 			}
 		}
 	}
-	return allowedInputs, maxInput
+
+	return out
+}
+
+// ProgramInput represents the MONAD input and the initial z value to use when reversing the MONAD.
+type ProgramInput struct {
+	MONAD, Z int
 }
--- week4/day_24_disasm.go
+++ week4/day_24_disasm.go
@@ -1,14 +1,19 @@
 package week4
 
+import "math"
+
+// InstBlock represents an instruction block in the program.
 type InstBlock interface {
 	Exec(int, int) int
-	AllWs(int) []int
+	GenInput(int) map[int]int
 }
 
+// AddInst represents an instruction block that performs an addition check.
 type AddInst struct {
 	A1, A2 int
 }
 
+// Exec executes the addition instruction block.
 func (pi *AddInst) Exec(w, z int) int {
 	if (z%26)+pi.A1 != w {
 		z = (z * 26) + w + pi.A2
@@ -16,48 +21,77 @@ func (pi *AddInst) Exec(w, z int) int {
 	return z
 }
 
-func (pi *AddInst) AllWs(z int) []int {
-	out := make([]int, 0, 9)
+// GenInput generates the inputs for the given target.
+func (pi *AddInst) GenInput(target int) map[int]int {
+	inputs := make(map[int]int, 9)
 	for w := 1; w <= 9; w++ {
-		out = append(out, pi.Exec(w, z))
+		pre := target - w - pi.A2
+		if pre%26 == 0 {
+			inputs[w] = pre / 26
+		}
 	}
-	return out
+	return inputs
 }
 
+// SubInst represents an instruction block that performs a subtraction check.
 type SubInst struct {
 	A1, A2 int
 }
 
-func (mi *SubInst) Exec(w, z int) int {
+// Exec executes the subtraction instruction block.
+func (si *SubInst) Exec(w, z int) int {
 	x := z
 	z /= 26
-	if (x%26)-mi.A1 != w {
-		z = (z * 26) + w + mi.A2
+	if (x%26)-si.A1 != w {
+		z = (z * 26) + w + si.A2
 	}
 	return z
 }
 
-func (si *SubInst) AllWs(z int) []int {
-	out := make([]int, 0, 9)
+// GenInput generates the inputs for the given target.
+func (si *SubInst) GenInput(target int) map[int]int {
+	maxInput := (target + 1) * 26
+	inputs := make(map[int]int, 9)
 	for w := 1; w <= 9; w++ {
-		out = append(out, si.Exec(w, z))
+		modTarget := 26 - (si.A1 + w)
+		inputs[w] = maxInput - modTarget
 	}
-	return out
+	return inputs
 }
 
 var ops = []InstBlock{
-	// &AddInst{13, 8},
-	// &AddInst{12, 16},
-	// &AddInst{10, 4},
-	// &SubInst{11, 1},
-	// &AddInst{14, 13},
-	// &AddInst{13, 5},
-	// &AddInst{12, 0},
-	// &SubInst{5, 10},
-	// &AddInst{10, 7},
+	&AddInst{13, 8},
+	&AddInst{12, 16},
+	&AddInst{10, 4},
+	&SubInst{11, 1},
+	&AddInst{14, 13},
+	&AddInst{13, 5},
+	&AddInst{12, 0},
+	&SubInst{5, 10},
+	&AddInst{10, 7},
 	&SubInst{0, 2},
 	&SubInst{11, 13},
 	&SubInst{13, 15},
 	&SubInst{13, 14},
 	&SubInst{11, 9},
+}
+
+// ExecuteProgram runs the instruction blocks consecutively.
+func ExecuteProgram(monad, z int) int {
+	var w int
+	for _, op := range ops {
+		w, monad = splitMSD(monad)
+		z = op.Exec(w, z)
+	}
+	return z
+}
+
+func splitMSD(input int) (int, int) {
+	x := float64(input)
+	lg := int(math.Pow10(int(math.Log10(x))))
+	if lg == 0 {
+		return 0, 0
+		// log.Fatalf("input contained a zero: %d", input)
+	}
+	return input / lg, input % lg
 }
